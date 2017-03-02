4a5c98516bff259235aa4e601a09fa9693500355
Philipp Ullmann <ullmann.philipp@gmail.com>
2017-03-02 18:11:46 +0100

    Database seed

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,10 @@
 class Term < ApplicationRecord
 	has_ancestry
+	belongs_to :user
+
+	def create_children_for!(terms, user)
+		terms.each do |term|
+			self.children.create! name: term, user: user
+		end
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+philipp = User.create! username: 'philipp', password: 'secret', password_confirmation: 'secret' 
+rene    = User.create! username: 'rene',    password: 'secret', password_confirmation: 'secret' 
+
+health = philipp.terms.create! name: 'Health'
+
+health.create_children_for! %w{Sleep Stress Exercise Help? Diet}, philipp
+
+health.children.find_by!(name: 'Sleep').create_children_for! ['33% of your life', 'Insomnia Tips', 'Insomnia Consequences'], rene
+
+health.children.find_by!(name: 'Stress').create_children_for! %w{Causes Solutions Effects}, philipp
+
+health.children.find_by!(name: 'Exercise').create_children_for! ['Warm up', 'Aerobic', 'Toning/Strenght', 'Stretching'], rene
+
+health.children.find_by!(name: 'Help?').create_children_for! ['Doctor', 'Dietician', 'Nutrition Aust'], philipp
+
+health.children.find_by!(name: 'Diet').create_children_for! ['Fruit', 'Vege', 'Breads & Cereals'], rene
