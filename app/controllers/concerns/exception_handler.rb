372b2397f59c1bc3667b058aadcdd3066665a729
Philipp Ullmann <ullmann.philipp@gmail.com>
2017-03-12 20:40:51 +0100

    Omit the argument if you don't care about it

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,7 +7,7 @@ module ExceptionHandler
       render json: { errors: [e.message] }, status: :not_found
     end
 
-    rescue_from Pundit::NotAuthorizedError do |e|
+    rescue_from Pundit::NotAuthorizedError do
       render json:   { errors: ['You are not authorized to perform this action'] },
              status: :unauthorized
     end
